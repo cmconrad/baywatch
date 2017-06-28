d4993d4fe7fac649e6e637d49de34f16d757182a
Caleb Conrad <cmconrad@anderson.edu>
2017-06-28 14:50:36 -0400

    Add working favorite button

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = {
         this.list=document.querySelector(selectors.listSelector)
 
         document.querySelector(selectors.formSelector).addEventListener("submit", this.handleSubmit.bind(this))
-        document.querySelector(selectors.buttonSelector).addEventListener("click",this.favChanger.bind(this))
     },
 
     handleSubmit(ev){
@@ -27,21 +26,25 @@ const app = {
         const item = document.createElement("li")
         const fav = document.createElement('button')
         fav.textContent = "Favorite"
-        fav.setAttribute("class","favButton")
+        fav.setAttribute("class","button")
         fav.setAttribute("id", this.max)
+        fav.addEventListener("click",this.favChanger.bind(this))
         myArray.push(flick.name)
         console.log(myArray)
         item.textContent = flick.name
         item.appendChild(fav)
         return item
     },
-    favChanger(button){
 
+    favChanger(ev){
+        li = ev.target.parentElement
+        li.style.backgroundColor = "red";
+        console.log(li)
+        console.log("clicked")
     }
 }
 
 app.init({
     formSelector:"form#flick-form",
     listSelector:"#flick-list",
-    buttonSelector:"favButton"
 })
