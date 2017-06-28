5dedaac1f61fca8d61584533c2f553289c289144
Caleb Conrad <cmconrad@anderson.edu>
2017-06-28 11:57:40 -0400

    Create a flick a object with a name and an id

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const app = {
     init: function(formSelector){
+        this.max = 0
         document
             .querySelector(formSelector)
-            .addEventListener("submit", this.handleSubmit)
+            .addEventListener("submit", this.handleSubmit.bind(this))
     },
 
     handleSubmit(ev){
         ev.preventDefault()
+        const f = ev.target
+        const flick = {
+            id: this.max + 1,
+            name: f.flickName.value
+        }
         const flickName = ev.target.flickName.value
-        console.log(flickName)
+        console.log(flick.name)
+        this.max ++
     },
 
 }
