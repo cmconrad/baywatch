b9b69b0c3ae449e2b99804a93cbc5b2c7bdc40d5
Caleb Conrad <cmconrad@anderson.edu>
2017-06-28 15:56:19 -0400

    Add delete button and fav functionality.

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const app = {
         const f = ev.target
         const flick = {
             id: this.max + 1,
-            name: f.flickName.value
+            name: f.flickName.value,
+            favStatus: false,
         }
 
         const listItem= this.renderListItem(flick)
@@ -24,24 +25,51 @@ const app = {
     },
     renderListItem(flick){
         const item = document.createElement("li")
+
         const fav = document.createElement('button')
         fav.textContent = "Favorite"
         fav.setAttribute("class","button")
         fav.setAttribute("id", this.max)
+        fav.style.position = "absolute"
+        fav.style.right = "10px"
         fav.addEventListener("click",this.favChanger.bind(this))
-        myArray.push(flick.name)
+
+
+        const del = document.createElement('button')
+        del.textContent = "Delete"
+        del.setAttribute("class","button")
+        //del.setAttribute("id",this.max)
+        del.style.position = "absolute"
+        del.style.right = "100px"
+        del.addEventListener("click",this.deleter.bind(this))
+
+        myArray.push(flick)
         console.log(myArray)
         item.textContent = flick.name
         item.appendChild(fav)
+        item.appendChild(del)
+        item.style.paddingBottom = "20px"
         return item
     },
 
     favChanger(ev){
         li = ev.target.parentElement
-        li.style.backgroundColor = "red";
-        console.log(li)
-        console.log("clicked")
-    }
+        if(li.style.backgroundColor == "red"){
+            li.style.backgroundColor = "white";
+            this.flick.favStatus = false;
+        }
+        else{
+             li.style.backgroundColor = "red";
+             this.flick.favStatus = true;
+        }
+       
+    },
+
+    deleter(ev){
+        f = ev.target.parentElement
+        f.remove(f)
+        // array
+    },
 }
 
 app.init({
