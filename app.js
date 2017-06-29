87113444e594226894a642f103f825de7156506e
Caleb Conrad <cmconrad@anderson.edu>
2017-06-28 21:31:02 -0400

    Working delete button, but the array is unaffected

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let myArray = []
 
 const app = {
     init: function(selectors){
+        this.myArray = []
         this.max = 0
         this.list=document.querySelector(selectors.listSelector)
 
         document.querySelector(selectors.formSelector).addEventListener("submit", this.handleSubmit.bind(this))
     },
-
+    
     handleSubmit(ev){
         ev.preventDefault()
         const f = ev.target
@@ -18,8 +18,7 @@ const app = {
         }
 
         const listItem= this.renderListItem(flick)
-        this.list.appendChild(listItem)
-
+        this.list.insertBefore(listItem, this.list.firstChild)
 
         this.max ++
     },
@@ -29,7 +28,7 @@ const app = {
         const fav = document.createElement('button')
         fav.textContent = "Favorite"
         fav.setAttribute("class","button")
-        fav.setAttribute("id", this.max)
+        fav.setAttribute("id", "f"+this.max)
         fav.style.position = "absolute"
         fav.style.right = "10px"
         fav.addEventListener("click",this.favChanger.bind(this))
@@ -38,13 +37,13 @@ const app = {
         const del = document.createElement('button')
         del.textContent = "Delete"
         del.setAttribute("class","button")
-        //del.setAttribute("id",this.max)
+        del.setAttribute("id","d"+this.max)
         del.style.position = "absolute"
         del.style.right = "100px"
         del.addEventListener("click",this.deleter.bind(this))
 
-        myArray.push(flick)
-        console.log(myArray)
+        this.myArray.push(flick)
+        console.log(this.myArray)
         item.textContent = flick.name
         item.appendChild(fav)
         item.appendChild(del)
@@ -68,6 +67,10 @@ const app = {
     deleter(ev){
         f = ev.target.parentElement
         f.remove(f)
+        const i = (ev.target.id.substring(1))
+        console.log(i)
+        this.myArray.splice(i-1,1)
+        this.max --
         // array
     },
 }
